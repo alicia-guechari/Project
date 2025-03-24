df6a9ffc59fc16290b361e3484af2d7d5e38c44c
alicia-guechari <a_guechari@estin.dz>
2025-03-24 15:22:02 +0100

    creating a superuser

--- Store/admin.py
+++ Store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Customer, Order , Category, Address, OrderItem, Cart, CartItem  # Import your models
 
 # Register your models here.
+
+
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(CartItem)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(OrderItem)
+admin.site.register(Address)
