ea67d4806278df7cf8696ef92fb6b99d53427828
Ayman <blidiayman47@gmail.com>
2025-03-25 10:07:16 +0100

    add password/reset/confirm, and some code organisation

--- Store/urls.py
+++ Store/urls.py
@@ -1,6 +1,3 @@
 from django.urls import path, include
 
-urlpatterns = [
-    path('auth/', include('dj_rest_auth.urls')),    
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
-]
+urlpatterns = []
