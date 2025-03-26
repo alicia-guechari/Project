caed2198c98bbba236d0097d20b657eb826db00c
Ayman <blidiayman47@gmail.com>
2025-03-26 10:43:23 +0100

    creat CRUD for categories for Admin

--- Store/urls.py
+++ Store/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('products/', ProductListCreateView.as_view()),
     path('products/<int:pk>/', ProductDetailView.as_view()),
+    
+    path('categories/', CategoryView.as_view()),
+    path('categories/<int:pk>/', CategoryDetailView.as_view()),
 ]
--- Store/views.py
+++ Store/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from .models import *
 from .serializers import *
 
-# ---------------------------------------Product----------------------------------------------
+# ---------------------------------------Product----------------------------------------------for admin
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
 
@@ -18,7 +18,7 @@ class ProductListCreateView(generics.ListCreateAPIView):
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
-    
+
     def get_permissions(self):
         if self.request.method == 'GET':
             return []
@@ -28,3 +28,16 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method == 'GET':
             return ProductListSerializer
         return ProductCreateSerializer
+
+
+# ---------------------------------------Category----------------------------------------------for admin
+class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = None
+    permission_classes = [permissions.IsAdminUser]
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAdminUser]
