b3615d3951cc5bfbb0c5d8e0013a12a3aaed1459
Ayman <blidiayman47@gmail.com>
2025-03-26 10:29:56 +0100

    implement pagination

--- website/settings.py
+++ website/settings.py
@@ -57,6 +57,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
 # ACCOUNT_AUTHENTICATION_METHOD = "email"
