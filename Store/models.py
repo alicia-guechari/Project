efa5727fc178fa1dd184de53cd109e056965de59
Ayman <blidiayman47@gmail.com>
2025-03-27 07:57:56 +0100

    some improvement in models

--- Store/models.py
+++ Store/models.py
@@ -1,65 +1,54 @@
 from django.db import models
-import datetime
-from django.contrib.auth.models import User , AbstractUser, Group, Permission # assuming dj's default user model
-from django.db.models.signals import post_save
-from django.conf import settings
-
-
-
-# Categories of Products
-class Category(models.Model):
-	name = models.CharField(max_length=255 , unique=True) # unique ensures that no categ can have the same name
-	description = models.TextField(blank=True, null=True) # charfield is for short txt like names , txtfield is for long txt like descrp
-
-	def __str__(self):
-		return self.name
-
+from django.contrib.auth.models import AbstractUser # assuming dj's default user model
 
 # address of users
 class Address(models.Model):
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100, blank=True, null=True)
-    street = models.CharField(max_length=255)
-    postal_code = models.CharField(max_length=20)
-	
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=10)
+
     def __str__(self):
         return f"{self.street}, {self.city}, {self.country}"
-    
 
 # Customers
 class Customer(AbstractUser):
-    phone = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
     address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
 
-	    
+# Categories of Products
+class Category(models.Model):
+	name = models.CharField(max_length=100 , unique=True) # unique ensures that no categ can have the same name
+	description = models.CharField(max_length=250,blank=True, null=True) # charfield is for short txt like names , txtfield is for long txt like descrp
+
+	def __str__(self):
+		return self.name
+
 # Products
 class Product(models.Model):
 	name = models.CharField(max_length=100, unique=True)
-	name = models.CharField(max_length=100 , unique=True)
-	price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+	description = models.CharField(max_length=250, blank=True, null=True )
+	price = models.DecimalField(decimal_places=2, max_digits=6)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
-	description = models.CharField(max_length=250, default='', blank=True, null=True )
 	image = models.ImageField(upload_to='media/product/', blank=True, null=True)
-	stoke = models.PositiveIntegerField()
+	stock = models.PositiveIntegerField()
 
 	def __str__(self):
 		return self.name # here the name of the product that appears in dj admin pannel
 
-
-
-# Customer Orders
+# Orders
 class Order(models.Model):
     CHOICES =[('pending', 'Pending'),('shipped', 'Shipped'),('delivered', 'Delivered'),('cancelled', 'Cancelled'),]
 
     user = models.ForeignKey(Customer, on_delete=models.CASCADE , related_name='orders')
     status = models.CharField(max_length=20, choices=CHOICES, default='pending')
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) #Stores the total cost of all items in the order
-    created_at = models.DateField(auto_now=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2) #Stores the total cost of all items in the order
     address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True) # the on_dele... ensures that the order won't be deleted id the addr is deleted
+    created_at = models.DateField(auto_now=True)
 
     def __str__(self):
-	    return f"Order {self.id} - {self.customer.username}"
+	    return f"Order {self.pk} - {self.user.username}"
 	# this is for readability in dj admin pannel
 
 class OrderItem(models.Model):
@@ -67,7 +56,10 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, editable=False) # editable prevents the user from manually editing this val
-    
+
+    class Meta:
+        unique_together = ['order', 'product'] 
+
     def save(self, *args, **kwargs):
         self.subtotal = self.quantity * self.product.price
         super().save(*args, **kwargs)
@@ -76,27 +68,28 @@ class OrderItem(models.Model):
         return f"{self.quantity} x {self.product.name} in Order {self.order.id}"
 
 
-
-# The Cart 
+# Cart 
 class Cart(models.Model):
     user = models.OneToOneField(Customer, on_delete=models.CASCADE)
 
+    def total_price(self):
+        return sum(item.subtotal for item in self.items.all())
+    
     def __str__(self):
         return f"Cart of {self.user.username}"
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
-    
+
+    class Meta:
+        unique_together = ['cart', 'product'] 
+
     def save(self, *args, **kwargs):
         self.subtotal = self.quantity * self.product.price
         super().save(*args, **kwargs)
-    
-    def total_price(self):
-        return sum(item.subtotal() for item in self.cartitem_set.all())
-    
-    def __str__(self):
-        return f"{self.quantity} x {self.product.name} in {self.cart.Customer.username}'s cart"
 
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in {self.cart.user.username}'s cart"
--- Store/views.py
+++ Store/views.py
@@ -1,16 +1,14 @@
 from rest_framework import generics, permissions, filters
-from rest_framework.pagination import PageNumberPagination
 from django_filters import rest_framework as django_filters
-from django.db.models import Q
 from .models import *
 from .serializers import *
 
-# ---------------------------------------Product----------------------------------------------for admin
+# ---------------------------------------Product----------------------------------------------
 class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
     category = django_filters.CharFilter(field_name="category__name", lookup_expr='iexact')
-    in_stock = django_filters.BooleanFilter(field_name="stoke", lookup_expr='gt', exclude=True)
+    in_stock = django_filters.BooleanFilter(field_name="stock", lookup_expr='gt', exclude=True)
 
     class Meta:
         model = Product
@@ -48,7 +46,7 @@ class ProductSearchView(generics.ListAPIView):
     filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ['name', 'description', 'category__name']
-    ordering_fields = ['price', 'name', 'stoke']
+    ordering_fields = ['price', 'name']
 
 # ---------------------------------------Category----------------------------------------------for admin
 class CategoryView(generics.ListCreateAPIView):
